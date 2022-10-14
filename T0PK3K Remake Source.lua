cb4e8e312b3a284c56d0d24679fc786b0041b3fc
nek205 <114011914+nek205@users.noreply.github.com>
2022-10-14 12:01:12 +0200

    Updated to Patch 29
    +Fixed Server Admin script not executing
    +Improved stability
    +Fixed lag on some devices
    -Removed herobrine

--- T0PK3K Remake Source.lua
+++ T0PK3K Remake Source.lua
@@ -243,7 +243,7 @@ TextLabel_2.BorderSizePixel = 0
 TextLabel_2.Position = UDim2.new(0, 6, 0, 29)
 TextLabel_2.Size = UDim2.new(1, -12, 0, 20)
 TextLabel_2.Font = Enum.Font.SourceSans
-TextLabel_2.Text = "Patch version 28"
+TextLabel_2.Text = "Patch version 29"
 TextLabel_2.TextColor3 = Color3.fromRGB(199, 199, 199)
 TextLabel_2.TextSize = 14.000
 
@@ -631,7 +631,7 @@ Script_18.Text = "FE Server Admin"
 Script_18.TextColor3 = Color3.fromRGB(199, 199, 199)
 Script_18.TextSize = 14.000
 Script_18.MouseButton1Click:Connect(function()
-	loadstring(game:HttpGet("loadstring(game:HttpGet('https://raw.githubusercontent.com/ONEReverseCard/My-Scripts/main/Netless%20Server%20Admin.md'))()"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/nek205/T0PK3K-Remake/Scripts/Server%20Admin"))()
 end)
 
 Script_19.Name = "Script"
@@ -809,7 +809,7 @@ TextLabel_13.BorderSizePixel = 0
 TextLabel_13.Position = UDim2.new(0, 6, 0, 6)
 TextLabel_13.Size = UDim2.new(1, -17, 0, 20)
 TextLabel_13.Font = Enum.Font.SourceSans
-TextLabel_13.Text = "Patch: 28"
+TextLabel_13.Text = "Patch: 29"
 TextLabel_13.TextColor3 = Color3.fromRGB(199, 199, 199)
 TextLabel_13.TextSize = 14.000
 
@@ -1058,5 +1058,5 @@ wait(1)
 print([[
 	created by nek205 ;)
 
-T0PK3K REMAKE PATCH 28
+T0PK3K REMAKE PATCH 29
 ]])
